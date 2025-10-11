e4d7c33a0aa9f398e5f6813cc17cf3ab3aaa959f
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2025-10-11 19:39:15 +0530

    playing songs

--- src/main/java/com/example/MusicPlayer/Controller/FileRestController.java
+++ src/main/java/com/example/MusicPlayer/Controller/FileRestController.java
@@ -1,12 +1,15 @@
 package com.example.MusicPlayer.Controller;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -53,5 +56,16 @@ public class FileRestController {
             return ResponseEntity.status(HttpStatus.OK).body(fileNames);
         }
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).build();
+    }
+
+    // playing
+    @GetMapping("/play/{song}")
+    public ResponseEntity<Resource> playSong(@PathVariable String song)throws MalformedURLException{
+        Path file = root.resolve(song);
+            Resource resource = new UrlResource(file.toUri());
+        if (!resource.exists() || !resource.isReadable()) {
+            throw new RuntimeException("Could not read song: " + song);
+        }
+        return ResponseEntity.ok().body(resource);
     }
 }
